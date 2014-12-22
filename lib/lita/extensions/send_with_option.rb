a93894644638a452575569edade696a944591244
Shu Sugimoto <shu@su.gimo.to>
2014-12-23 01:43:03 +0900

    extend core classes to handle option hash when sending a message

--- lib/lita/extensions/send_with_option.rb
+++ lib/lita/extensions/send_with_option.rb
@@ -1,10 +1,40 @@
-module Lita
-  module Extensions
-    class SendWithOption
+# extend Lita core classes
+
+class ::Lita::Message
+  def reply(*strings, option: nil)
+    @robot.send_messages(source, *strings, option: option)
+  end
+
+  def reply_privately(*strings, option: nil)
+    private_source = source.clone
+    private_source.private_message!
+    @robot.send_messages(private_source, *strings, option: option)
+  end
+
+  def reply_with_mention(*strings, option: nil)
+    @robot.send_messages_with_mention(source, *strings, option: option)
+  end
+end
+
+class ::Lita::Robot
+  def send_messages(target, *strings, option: nil)
+    if @adapter.respond_to? :send_messages_with_option
+      adapter.send_messages_with_option(target, strings.flatten, option: option)
+    else
+      adapter.send_messages(target, strings.flatten)
+    end
+  end
+  alias_method :send_message, :send_messages
+
+  def send_messages_with_mention(target, *strings, option: nil)
+    return send_messages(target, *strings, option: option) if target.private_message?
+
+    mention_name = target.user.mention_name
+    prefixed_strings = strings.map do |s|
+      "#{adapter.mention_format(mention_name).strip} #{s}"
     end
 
-    # If your extension needs to register with a Lita hook, uncomment the
-    # following line and change the hook name to the appropriate value:
-    # Lita.register_hook(:hook_name, SendWithOption)
+    send_messages(target, *prefixed_strings, option: option)
   end
+  alias_method :send_message_with_mention, :send_messages_with_mention
 end
